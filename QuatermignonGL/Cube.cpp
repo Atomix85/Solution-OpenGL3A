dd1da6bdad59b4bc0a62626bb524c12efa319d01
brete <bretellealan@hotmail.fr>
2020-05-22 00:30:22 +0200

    correction

--- QuatermignonGL/Cube.cpp
+++ QuatermignonGL/Cube.cpp
@@ -9,17 +9,37 @@ Cube::Cube(float x, float y, float z, Quaternion rotation)
 void Cube::draw()
 {
 	GLfloat* matrix;
-	GLfloat matrixIdentity[] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
+	//GLfloat matrixIdentity[] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
 	glPushMatrix();
 	//glLoadIdentity();
 	matrix = _rotation.quaternion2Matrix();
 
-	glLoadMatrixf(matrix);
-	glMultMatrixf(matrixIdentity);
+	glMultMatrixf(matrix);
+	//glMultMatrixf(matrixIdentity);
 
 	free(matrix);
 
 	glutSolidCube(1.0f);
+
+	//HELPER
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+		glVertex3f(0, 0, 0);
+		glVertex3f(2, 0, 0);
+	glEnd();
+
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 2, 0);
+	glEnd();
+
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 2);
+	glEnd();
+
 	glPopMatrix();
 
 }
